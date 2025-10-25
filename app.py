37c993193c7a1a73cb54e6892265481ba35137cd
rhehdud <rhehdud3@gmail.com>
2025-10-25 09:02:14 +0900

    이메일 정규식 추가

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+import re
 from flask import Flask, render_template, request, jsonify
 from openai import OpenAI
 
 client_adult = OpenAI(api_key='')
 client_child = OpenAI(api_key='')
 model_name = "gpt-5-nano"
+
+EMAIL_RE = re.compile(
+    r"^[A-Za-z0-9.!#$%&'*+/=?^_`{|}~-]+"
+    r"@"
+    r"[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?"
+    r"(?:\.[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?)*$"
+)
+
+def is_valid_email(addr: str) -> bool:
+    return bool(EMAIL_RE.fullmatch(addr))
 
 app = Flask(__name__)
 
@@ -14,8 +25,20 @@ def home():
 @app.route('/signup', methods=['POST'])
 def signup():
    data = request.get_json()
-   id = data.get('id')
+   name = data.get('name')
+   email = data.get('email')
    password = data.get('password')
+   password_confirm = data.get('password_confirm')
+   child_name = data.get('child_name')
+   child_age = data.get('child_age')
+
+   #이메일 확인 정규식
+   if not is_valid_email(email):
+      return jsonify({'result': 'fail', 'msg': '잘못된 이메일 형식'})
+   
+   #비밀번호 확인 로직
+   if (password != password_confirm):
+      return jsonify({'result': 'fail', 'msg': '비밀번호 불일치'})
 
    #데이터 베이스에 저장 로직
    return jsonify({'result': 'success', 'msg': '회원가입 성공'})
@@ -23,7 +46,7 @@ def signup():
 @app.route('/login', methods=['POST'])
 def login():
    data = request.get_json()
-   id = data.get('id')
+   email = data.get('email')
    password = data.get('password')
 
    #데이터 베이스와 비교 로직
@@ -31,16 +54,17 @@ def login():
 
 @app.route('/adult', methods=['POST'])
 def chat_adult():
+   data = request.get_json()
    response = client_adult.responses.create(
       model=model_name,
       input=[
          {
             'role': 'developer',
-            'content': '시스템 프롬프트'
+            'content': '시스템 프롬프트 (아이와 대화)'
          },
          {
             'role': 'user',
-            'content': '프롬프트'
+            'content': data.get('prompt')
          }
         ]
     )
@@ -48,16 +72,17 @@ def chat_adult():
 
 @app.route('/child', methods=['POST'])
 def chat_child():
+   data = request.get_json()
    response = client_child.responses.create(
       model=model_name,
       input=[
          {
             'role': 'developer',
-            'content': '시스템 프롬프트'
+            'content': '시스템 프롬프트 (루틴 제공, 부모에게 리포트 제공)'
          },
          {
             'role': 'user',
-            'content': '프롬프트'
+            'content': data.get('prompt')
          }
         ]
     )
